6af2e6778331054724f0f474667d48c33a700412
godloveevin <godloveevin@yeah.net>
2019-08-09 00:58:41 +0800

    修改Api，Backstage模块的Index控制器，并配置默认的访问模块Home

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+
+    /* 数据库设置 */
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'ebuy',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'eb_',    // 数据库表前缀
+
+    'DEFAULT_MODULE'=>'Home',// 设置默认的访问模块为Home
+    'MODULE_ALLOW_LIST'=>array('Api','Home','Backstage'),// 设置允许访问的模块
 );
--- README.md
+++ README.md
@@ -3,6 +3,6 @@ This is a mall project with PHP and MySQL to build source code,also use ThinkPHP
 # Information about author
 Email:godloveevin@yeah.net Origo:China yichun City:hangzhou
 
-#Project directory structure
+# Project directory structure
 1.http://domain.com-->wwwroot
 2.wwwroot和框架目录在同一级目录，wwwroot目录不存放过多的代码，出于安全考虑
