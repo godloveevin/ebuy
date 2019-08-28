c0521b5007276766c4a14db120f89946024ae57c
godloveevin <godloveevin@yeah.net>
2019-08-28 19:38:31 +0800

    完善aja删除分类的功能

--- Application/Backstage/Controller/CategoryController.class.php
+++ Application/Backstage/Controller/CategoryController.class.php
@@ -79,18 +79,35 @@ class CategoryController extends BaseController{
 
     // ajax删除删除分类信息
     public function dels(){
-        $cat_id = I('get.cat_id');
+        $cat_id = intval(I('get.cat_id'));
         if(!$cat_id){
             $this->ajaxReturn(array('status'=>0,'msg'=>'删除失败'));
         }
         // 删除分类逻辑
         // 如果有子分类，则禁止删除
-        // 如果该分类下有商品的话，禁止删除
         $model = D('Category');
         if($model->getChildCat($cat_id))
             $this->ajaxReturn(array('status'=>0,'msg'=>'删除失败，有子分类禁止删除'));
+
+        // 如果该分类下有商品的话，禁止删除
         if(D('Goods')->getGoodsInfoByCatId($cat_id))
             $this->ajaxReturn(array('status'=>0,'msg'=>'删除失败，分类下有商品禁止删除'));
+
+        // 该分类与子分类存在扩展分类记录的，禁止删除
+        // 组装分类id
+        $cat_ids[] = $cat_id; // 本分类id
+        // 组装本分类下的子分类
+        $sub_cat_ids = $model->getTreeCatInfoNoCached("cat_id,cat_name,parent_id",$cat_id);
+        foreach ($sub_cat_ids as $k=>$v) {
+            $cat_ids[] = $v['cat_id'];
+        }
+        // 根据扩展分类获取符合条件的商品id集
+        $good_cate_infos = M('goods_category')->group('good_id')
+            ->where(array('cat_id'=>array('in',$cat_ids)))->select();
+        if($good_cate_infos)
+            $this->ajaxReturn(array('status'=>0,'msg'=>'删除失败，分类与子分类存在扩展分类记录的，禁止删除'));
+
+        // 执行删除分类操作
         if(!$model->dels($cat_id))
             $this->ajaxReturn(array('status'=>0,'msg'=>'删除失败，系统错误'));
         $this->ajaxReturn(array('status'=>1,'msg'=>'ok!'));
